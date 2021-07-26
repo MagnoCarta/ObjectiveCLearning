e1df4f29655ea4ba8c776e436d009d1e2c405278
Gilberto Magno <gilbertomagno@MacBook-Pro-de-Gilberto.local>
2021-07-26 09:20:44 -0300

    Fixing path header

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MathSupport",
-            dependencies: []),
+            dependencies: [],
+            publicHeadersPath: "Sources/MathSupport"),
         .testTarget(
             name: "MathSupportTests",
             dependencies: ["MathSupport"]),
