2901d101134537b801e6925b6139b7b3e92f0356
Gilberto Magno <gilbertomagno@MacBook-Pro-de-Gilberto.local>
2021-07-26 08:55:33 -0300

    Adding publicHeader on target package.swift

--- MathSupport/Package.swift
+++ MathSupport/Package.swift
@@ -9,21 +9,16 @@ let package = Package(
         .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MathSupport",
             targets: ["MathSupport"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MathSupport",
-            dependencies: []),
+            dependencies: [],
+            publicHeadersPath: "Sources/MathSupport")
+        ,
         .testTarget(
             name: "MathSupportTests",
             dependencies: ["MathSupport"]),
